e4d6a9e25f383adaf736bee57dc3cb9a46b8c299
Alquila <athallenberg@gmail.com>
2023-09-06 14:02:24 +0200

    made node file

---
+++ node.py
@@ -0,0 +1,5 @@
+import numpy as np
+import typing
+
+## Make class to handle node. Should be able to handle both internal nodes and leafs
+# node should have a: parent, children, 
--- suffixtree.py
+++ suffixtree.py
@@ -1,1 +1,5 @@
+import numpy as np
+import typing
 
+
+## Should using node class be able to create tree
