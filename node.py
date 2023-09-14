652bc147105171fdbf1bb1c40284988763e75603
Alquila <athallenberg@gmail.com>
2023-09-14 10:07:52 +0200

    created internal node

--- node.py
+++ node.py
@@ -2,18 +2,17 @@ import numpy as np
 import typing
 
 class Node():
-    def __init__(self, name, depth = 0, startInd = None, endInd = None, 
+    def __init__(self, name, depth = 0, start_idx = 0, end_idx = None,
                  parent = None, children = None):
         self.name = name
         self.depth = depth
         self.parent = parent
         self.children = children
-        self.startInd = startInd
-        self.endInd = endInd
+        self.start_idx = start_idx
+        self.end_idx = end_idx
 
     def __str__(self):
         return f"{self.name}({self.depth})"
-
 
 
 ## Make class to handle node. Should be able to handle both internal nodes and leafs
--- suffixtree.py
+++ suffixtree.py
@@ -3,7 +3,9 @@ import typing
 from node import Node
 
 
-class NaiveSuffixTree():
+## TODO make new node function
+
+class NaiveSuffixTree:
     """ Builds a Naive Suffix Tree
     """
 
@@ -20,15 +22,30 @@ class NaiveSuffixTree():
         len_seq = len(self.sequence)
         for i in range(len_seq):
             if current_node.children != []:
-                for j in current_node.children:
-                    traverseTree()
+                for child in current_node.children:
+                    res_idx = traverseTree(i, child.startIndex)
+                    if res_idx == -1:
+                        # go to Child
+                        current_node = child
+                        ## continue traverse and/or insert node
+                    elif res_idx > 0:
+                        # insert new node
+                        new_internal_node = Node(name="internal", depth=(current_node.depth + 1),
+                                                 start_idx=(current_node.end_idx + 1),
+                                                 end_idx=(current_node.end_idx + res_idx), parent=current_node,
+                                                 children=child)
+                        current_node.children.remove(child)
+                        current_node.children.append(new_node)
+                        leaf_number += 1
+                        ## Create new child note and change start index of existing child node
+
             elif current_node.name == "root":
                 new_node = Node(leaf_number, 1, i, (len_seq - 1), parent=current_node)
-                current_node.children.append(new_node);
-                leaf_number += 1;
+                current_node.children.append(new_node)
+                leaf_number += 1
 
-    def traverseTree(self, suffix_idx, node_idx, res):
-        if self.sequence[suffix_idx] != self.sequence[node_idx]:
+    def traverseTree(self, suffix_idx, elem_idx, res=0):
+        if self.sequence[suffix_idx] != self.sequence[elem_idx]:
             return res
         else:
-            return traverseTree(suffix_idx + 1, node_idx + 1, res + 1)
+            return traverseTree(suffix_idx + 1, elem_idx + 1, res + 1)
