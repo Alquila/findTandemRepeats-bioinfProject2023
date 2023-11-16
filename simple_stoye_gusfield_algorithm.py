0aee3bb3b65a79f5bff6f46503663c87e80c8474
Sara-Elna <elna.elisa@gmail.com>
2023-11-16 11:06:22 +0100

    notes for SG

--- simple_stoye_gusfield_algorithm.py
+++ simple_stoye_gusfield_algorithm.py
@@ -38,8 +38,53 @@ def basic_stoye_gusfield(node: Node, depthfirst_to_suffix, sequence, testing):
 
     return tandem_repeat_list
 
-def stoye_gusfield(node: Node, depthfirst_to_suffix, sequence, testing):
-    
-    #TODO - the code
 
-    return None
+
+
+
+def stoye_gusfield(node: Node, depthfirst_to_suffix, sequence, testing):
+    if testing: print('str(node) = ' + str(node))
+
+    # create empty list for tandem repeats
+    tandem_repeat_list = []
+
+    #TODO - call LL(v) method to get small LL'(v) and large LL(v') and v'
+    # replaces get suffix/leaf list below 
+
+    # get suffix/leaf list
+    suffix_list = [] # This corresponds to the LL(v) in the slides
+    depthfirst_list = node.depth_first # if it is an internal node or the root this is a min-max range
+    if node.type != "leaf":
+        depth_min = depthfirst_list[0] # the minimum depth for the internal node
+        depth_max = depthfirst_list[1] # the maximum depth for the internal node
+        for index in range(depth_min, depth_max+1):
+            suffix_list.append(depthfirst_to_suffix[index])
+    else:
+        return
+    
+    # for loop runs trhough small LL'(v) and make corresponding checks
+    # i loops through small LL'(v)
+    # j loops through LL(v) with checks
+    #   j == i + D(v) and S[i] != S[i+2D(v)] --> j is in small
+    # j loops through LL(v') with checks ??? 
+    #   j == i - D(v) and S[i-D(v)] != S[i+D(v)] --> j is in large
+
+    for i in suffix_list:
+        for j in suffix_list:
+            if i < j and j == i + node.string_depth and sequence[i] != sequence[i + 2 * node.string_depth] and node.string_depth > 1:
+                if testing:
+                    print('here is i < j and j == i + |a| and S[i] != S[i+2*|a|]')
+                    print('i : ' + str(i))
+                    print('j : ' + str(j))
+                tandem_repeat = (i, node.string_depth, 2)
+                if testing: print('!!!' + str(tandem_repeat))
+                tandem_repeat_list.append(tandem_repeat)
+                if testing: print('the list!!! : ' + str(tandem_repeat_list))
+
+    if node.children:
+        for child in node.children.values():
+            returned_tandem_repeats = basic_stoye_gusfield(child, depthfirst_to_suffix, sequence, testing)
+            if returned_tandem_repeats != None and returned_tandem_repeats != []:
+                tandem_repeat_list.append(returned_tandem_repeats)
+
+    return tandem_repeat_list
